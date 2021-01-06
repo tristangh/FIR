7c3969f40cb46b600b4ea857cb8a978361ec8701
Tristan <tristan.ghodsy@isep.fr>
2021-01-06 13:41:06 +0100

    Pages (Activity & Layout) initialisation
    Created all pages from flowchart & specification doc

---
+++ app/src/main/java/com/example/MyTreasury/Account.java
@@ -0,0 +1,18 @@
+package com.example.MyTreasury;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Account extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accountview);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/MyTreasury/Dashboard.java
@@ -0,0 +1,30 @@
+package com.example.MyTreasury;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Dashboard extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+    }
+
+    public void onButtonClick(View v) {
+        //Log.d("Dashboard error : ", String.valueOf(v.getId()));
+        if (v.getId() == R.id.loginpageshow_button) {
+            Intent i = new Intent(Dashboard.this, Login.class);
+            startActivity(i);
+        }
+
+    }
+
+
+}
