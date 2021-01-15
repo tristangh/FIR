ca419275f70a472708552b1c38748010165f2f46
Utilisateur <nithyaeswari001@gmail.com>
2021-01-15 07:19:39 +0100

    LoginPage register page added

--- app/src/main/java/com/example/MyTreasury/Login.java
+++ app/src/main/java/com/example/MyTreasury/Login.java
@@ -1,14 +1,39 @@
 package com.example.MyTreasury;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Login extends Activity {
+
+    TextView register;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginview);
+        setContentView(R.layout.login);
+
+        register = findViewById(R.id.register);
+        back = findViewById(R.id.button3);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,Register.class));
+                finish();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,Register.class));
+                finish();
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/MyTreasury/Register.java
+++ app/src/main/java/com/example/MyTreasury/Register.java
@@ -2,13 +2,58 @@ package com.example.MyTreasury;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Register extends Activity {
+
+    Spinner association_spinner;
+    String item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerview);
+
+        association_spinner = findViewById(R.id.spinner_association);
+
+        setSpinnerItems();
+
+    }
+
+    private void setSpinnerItems(){
+
+        final ArrayList<String> spinnerArray = new ArrayList<String>();
+        spinnerArray.add("Sportive");
+        spinnerArray.add("Charity");
+        spinnerArray.add("Others");
+
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                spinnerArray);
+        association_spinner.setAdapter(spinnerArrayAdapter);
+
+
+        association_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.black));
+
+                item = spinnerArray.get(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
 
