661a01b27871bbd5d21c3d58c43e74d24ddba653
Tristan <tristan.ghodsy@isep.fr>
2021-01-24 17:36:40 +0100

    Added AddMember, AccountEdit & Account Views

--- app/src/main/java/com/example/MyTreasury/AccountEdit.java
+++ app/src/main/java/com/example/MyTreasury/AccountEdit.java
@@ -1,14 +1,114 @@
 package com.example.MyTreasury;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AccountEdit extends Activity {
+    Spinner association_spinner,asso_mem;
+    String item;
+    Button addMemberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_editview);
+
+        association_spinner = findViewById(R.id.spinner_association);
+        asso_mem = findViewById(R.id.spinner_mem);
+        addMemberButton = findViewById(R.id.addmember_button);
+
+
+        setSpinnerItems();
+        setSpinnerMem();
+
+
+        addMemberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(AccountEdit.this, AddMember.class);
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+
+
+
+    private void setSpinnerItems(){
+
+        final ArrayList<String> spinnerArray = new ArrayList<String>();
+        spinnerArray.add("Sportive");
+        spinnerArray.add("Charity");
+        spinnerArray.add("Others");
+
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                spinnerArray);
+        association_spinner.setAdapter(spinnerArrayAdapter);
+
+
+        association_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.black));
+
+                item = spinnerArray.get(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+
+    private void setSpinnerMem(){
+
+        final ArrayList<String> spinnerArray = new ArrayList<String>();
+        spinnerArray.add("Nithya");
+        spinnerArray.add("Tristan");
+        spinnerArray.add("Amine");
+        spinnerArray.add("Paul");
+        spinnerArray.add("Jean");
+
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                spinnerArray);
+        asso_mem.setAdapter(spinnerArrayAdapter);
+
+
+        asso_mem.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.black));
+
+                item = spinnerArray.get(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
 
